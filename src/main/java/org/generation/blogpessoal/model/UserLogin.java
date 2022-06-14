6db3a5a60208a106c9045c2974b2a0e886bd34d4
Gabriele Sá <93798476+psgabriele@users.noreply.github.com>
2022-06-14 19:22:36 -0300

    Deploy

---
+++ src/main/java/org/generation/blogpessoal/model/UserLogin.java
@@ -0,0 +1,77 @@
+package org.generation.blogpessoal.model;
+
+public class UserLogin {
+	
+	private long id;
+
+	private String nome;
+	
+	private String usuario;
+	
+	private String senha;
+	
+	private String foto;
+	
+	private String token;
+	
+	public UserLogin(long id, String nome, String usuario, String senha, String foto, String token) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+		this.token = token;
+	}
+	
+	public UserLogin() {
+		
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+}
